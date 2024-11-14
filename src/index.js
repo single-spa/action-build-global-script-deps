f3d481271405a15a96c205f22652adeb350d85d9
Joel Denning <joeldenning@gmail.com>
2024-11-13 22:54:05 -0700

    Initial implementation (#1)
    * Initial implementation
    
    * Include dist folder
    
    * Auto build
    
    * Update readme
    
    * Add packageManager field

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { getInput, setFailed } from "@actions/core";
+import { buildGlobalScripts } from "@single-spa/build-global-script-deps";
+import { readFile } from "node:fs/promises";
+
+let globalScriptsStr;
+try {
+  globalScriptsStr = await readFile(getInput("global-scripts"));
+} catch (err) {
+  console.error(err);
+  setFailed(
+    `Unable to read global scripts template from filePath '${getInput("global-scripts")}'`,
+  );
+}
+
+let globalScriptsJson;
+try {
+  globalScriptsJson = JSON.parse(globalScriptsStr);
+} catch (err) {
+  console.error(err);
+  setFailed(
+    `global-scripts file contains invalid json JSON. File path '${getInput("global-scripts")}'`,
+  );
+}
+
+if (
+  !globalScriptsJson["global-scripts"] ||
+  !Array.isArray(globalScriptsJson["global-scripts"])
+) {
+  setFailed(
+    `global-scripts JSON file must contain top-level property called 'global-scripts' that contains an array of strings`,
+  );
+}
+
+await buildGlobalScripts({
+  globalScripts,
+  outputDir: getInput("output-dir"),
+});
